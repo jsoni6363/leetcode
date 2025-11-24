ef085a440373c0de9020c5923e131cd698af0d09
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-11-25 00:55:43 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.34%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,30 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        stack<pair<TreeNode*,TreeNode*>> s;
+        s.push({p,q});
 
-        // Stack stores pairs of nodes from both trees
-        stack<pair<TreeNode*, TreeNode*>> st;
-        st.push({p, q});
+        while(!s.empty()){
+            auto [node1,node2] = s.top();
+            s.pop();
 
-        while (!st.empty()) {
+            if(!node1 && !node2) continue;
+            if(!node1 || !node2 || node1->val!=node2->val) return false;
 
-            // Take the top pair
-            auto [node1, node2] = st.top();
-            st.pop();
-
-            // Case 1: both nodes are NULL → continue
-            if (!node1 && !node2) continue;
-
-            // Case 2: one is NULL or values differ → not same
-            if (!node1 || !node2 || node1->val != node2->val)
-                return false;
-
-            // Case 3: push children to compare later
-            st.push({node1->right, node2->right});
-            st.push({node1->left, node2->left});
+            s.push({node1->left,node2->left});
+            s.push({node1->right, node2->right});
         }
-
-        // All nodes matched → trees are the same
         return true;
+
     }
 };
