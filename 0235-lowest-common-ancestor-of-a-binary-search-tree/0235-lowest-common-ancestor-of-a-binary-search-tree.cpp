8cc0d3d64c61f05cc357642b5bbb4081fac717ee
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-11-27 00:24:39 +0530

    Time: 18 ms (87.74%), Space: 23.6 MB (33.08%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,32 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-
-        // Traverse the BST starting from the root
-        while (root != nullptr) {
-
-            // CASE 1:
-            // If BOTH p and q have values smaller than the current root,
-            // they both lie in the LEFT subtree.
-            if (p->val < root->val && q->val < root->val)
-                root = root->left;
-
-            // CASE 2:
-            // If BOTH p and q have values bigger than the current root,
-            // they both lie in the RIGHT subtree.
-            else if (p->val > root->val && q->val > root->val)
-                root = root->right;
-
-            // CASE 3:
-            // If the above two conditions do NOT match:
-            // It means p and q lie on DIFFERENT sides of root,
-            // OR root is equal to one of p or q.
-            // Therefore, root is the SPLIT POINT → LCA found.
-            else
-                return root;
+        while(root!=NULL){
+            if(p->val<root->val && q->val<root->val)
+            root=root->left;
+            else if(p->val>root->val && q->val>root->val)
+            root=root->right;
+            else 
+            return root;
         }
-
-        // If somehow the tree is empty
-        return nullptr;
+        return NULL;
     }
 };
