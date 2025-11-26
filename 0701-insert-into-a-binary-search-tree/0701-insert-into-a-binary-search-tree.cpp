26e5803dc55e95fff3dff8f01479c0d2fce2accf
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-11-27 00:47:52 +0530

    Time: 0 ms (100%), Space: 59.4 MB (8.28%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -1,36 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root) 
+        return new TreeNode(val);
 
-        // BASE CASE:
-        // If we reach a NULL position, that means
-        // this is the correct empty spot where the new value should go.
-        // So create and return a new node.
-        if (!root) {
-            return new TreeNode(val);
-        }
+        if(root->val<val)
+        root->right=insertIntoBST(root->right,val);
+        else
+        root->left=insertIntoBST(root->left,val);
 
-        // RECURSIVE CASE:
-        // If the value to insert is GREATER than the current node,
-        // we must go to the RIGHT subtree.
-        if (val > root->val) {
-
-            // Recursively insert into right subtree
-            // and update the right child pointer.
-            root->right = insertIntoBST(root->right, val);
-        }
-
-        // Otherwise, val is LESS than or EQUAL to root->val,
-        // so we go to the LEFT subtree.
-        else {
-
-            // Recursively insert into left subtree
-            // and update the left child pointer.
-            root->left = insertIntoBST(root->left, val);
-        }
-
-        // Return the unchanged root pointer
-        // so that parent nodes can reconnect properly.
         return root;
-    }
+    };
 };
