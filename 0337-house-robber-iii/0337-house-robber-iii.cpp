d7e25260e2bfe72fb195ddbf1be7f2169adc5e21
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-12-03 23:38:42 +0530

    Time: 4 ms (46.2%), Space: 25.3 MB (38.53%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -9,56 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-    // cache[node] = maximum money we can rob from the subtree rooted at 'node'
-    unordered_map<TreeNode*, int> cache;
+unordered_map<TreeNode*,int> cache;
+private:
+int dfs(TreeNode* root){
+    if(cache.find(root)!=cache.end())
+    return cache[root];
 
-public:
-    int rob(TreeNode* root) {
-        // For a null node (no house), the maximum money is 0.
-        cache[nullptr] = 0;
+    int takeroot=root->val;
 
-        // Start the recursion from the root node.
-        return dfs(root);
+    if(root->left){
+        takeroot += dfs(root->left->left);
+        takeroot += dfs(root->left->right);
     }
 
-private:
-    int dfs(TreeNode* root) {
-        // If we already computed the answer for this node,
-        // return it directly from the cache (no need to recompute).
-        if (cache.find(root) != cache.end()) {
-            return cache[root];
-        }
+    if(root->right){
+        takeroot += dfs(root->right->left);
+        takeroot += dfs(root->right->right);
+    }
 
-        // ---- Option 1: Rob this node (root) ----
-        // Start with the money in the current house.
-        int takeRoot = root->val;
+    int skiproot= dfs(root->left) + dfs(root->right);
 
-        // If there is a left child, we cannot rob it,
-        // but we can try to rob its children (grandchildren of root).
-        if (root->left) {
-            takeRoot += dfs(root->left->left);  // left-left grandchild
-            takeRoot += dfs(root->left->right); // left-right grandchild
-        }
-
-        // Similarly for the right child: skip it but consider its children.
-        if (root->right) {
-            takeRoot += dfs(root->right->left);  // right-left grandchild
-            takeRoot += dfs(root->right->right); // right-right grandchild
-        }
-
-        // ---- Option 2: Do NOT rob this node ----
-        // If we skip this node, we are free to choose the best from its children.
-        int skipRoot = dfs(root->left) + dfs(root->right);
-
-        // The best answer for this node is the maximum of the two options.
-        int best = max(takeRoot, skipRoot);
-
-        // Store the answer in the cache so we don't recompute it later.
-        cache[root] = best;
-
-        // Return the best result for this subtree.
-        return best;
+    int best=max(takeroot,skiproot);
+    cache[root]=best;
+    return best;
+}  
+public:
+    int rob(TreeNode* root) {
+        cache[NULL]=0;
+        return dfs(root);
     }
 };
