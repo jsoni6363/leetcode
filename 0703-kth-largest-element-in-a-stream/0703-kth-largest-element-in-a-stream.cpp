08f4fd8f717acc9a8c798a2c44f83cf8d9bc7fc2
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-12-22 14:53:30 +0530

    Time: 7 ms (61.61%), Space: 32.9 MB (93.95%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,54 +1,29 @@
 class KthLargest {
-private:
-
-    // Min heap that stores ONLY the k largest elements
-    // The smallest element in this heap will be the k-th largest overall
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-
-    // Stores the value of k
-    int k;
-
 public:
+priority_queue<int,vector<int>,greater<int>> minHeap;
+int k;
 
-    // ============================
-    // CONSTRUCTOR
-    // ============================
     KthLargest(int k, vector<int>& nums) {
+        this->k=k;
 
-        // Save k
-        this->k = k;
-
-        // Insert initial numbers into the heap
-        for (int num : nums) {
-
-            // Step 1: Push the number into the heap
+        for(auto num:nums){
             minHeap.push(num);
-
-            // Step 2: If heap size exceeds k,
-            // remove the smallest element
-            // This ensures heap always has ONLY k largest elements
-            if (minHeap.size() > k) {
-                minHeap.pop();
-            }
-        }
-    }
-
-    // ============================
-    // ADD FUNCTION
-    // ============================
-    int add(int val) {
-
-        // Step 1: Add the new value to the heap
-        minHeap.push(val);
-
-        // Step 2: If heap size becomes greater than k,
-        // remove the smallest element
-        if (minHeap.size() > k) {
+            if(minHeap.size()>k)
             minHeap.pop();
         }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size()>k)
+        minHeap.pop();
 
-        // Step 3: The top of the min heap
-        // is the k-th largest element
         return minHeap.top();
     }
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
