06f63f2cc1671ee4cf531c0e9a324cbbf398e776
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-12-27 23:47:57 +0530

    Time: 0 ms (100%), Space: 14 MB (95.09%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-
-    // Helper function to sort trips by start location
-    static bool compare(vector<int>& a, vector<int>& b) {
-        return a[1] < b[1];  // sort by startLocation
+    static bool compare(vector<int>&a , vector<int>& b){
+        return a[1]<b[1];
     }
-
     bool carPooling(vector<vector<int>>& trips, int capacity) {
+        sort(trips.begin(),trips.end(),compare);
+        int n=trips.size();
+        for(int i=0;i<n;i++){
+            int currPass=trips[i][0];
 
-        // Step 1: Sort trips based on starting point
-        sort(trips.begin(), trips.end(), compare);
-
-        // Step 2: Go through each trip one by one
-        for (int i = 0; i < trips.size(); i++) {
-
-            // Passengers of current trip
-            int currentPassengers = trips[i][0];
-
-            // Step 3: Check all previous trips
-            for (int j = 0; j < i; j++) {
-
-                // If previous trip has NOT ended yet,
-                // its passengers are still in the car
-                if (trips[j][2] > trips[i][1]) {
-                    currentPassengers += trips[j][0];
-                }
+            for(int j=0;j<i;j++){
+                if(trips[j][2]>trips[i][1])
+                currPass+=trips[j][0];
             }
-
-            // Step 4: Check capacity
-            if (currentPassengers > capacity) {
-                return false;  // capacity exceeded
-            }
+            if(currPass>capacity)
+            return false;
         }
-
-        // If we never exceeded capacity
         return true;
     }
 };
