5c57741ab946517bab9267e54089cee7ecd2e136
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2025-12-27 23:58:34 +0530

    Time: 2 ms (41.82%), Space: 14.1 MB (95.09%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -4,18 +4,50 @@ public:
         return a[1]<b[1];
     }
     bool carPooling(vector<vector<int>>& trips, int capacity) {
+         // Step 1: Sort trips by starting location
+        // trips[i][1] is the start point
         sort(trips.begin(),trips.end(),compare);
-        int n=trips.size();
-        for(int i=0;i<n;i++){
-            int currPass=trips[i][0];
 
-            for(int j=0;j<i;j++){
-                if(trips[j][2]>trips[i][1])
-                currPass+=trips[j][0];
+        // Min heap to store ongoing trips
+        // Each element = {endLocation, numberOfPassengers}
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > minHeap;
+
+        // Current number of passengers in the car
+        int currentPassengers = 0;
+
+        // Step 2: Go through each trip
+        for (int i = 0; i < trips.size(); i++) {
+
+            int passengers = trips[i][0];
+            int start = trips[i][1];
+            int end = trips[i][2];
+
+            // Step 3: Remove passengers who have already reached destination
+            if (!minHeap.empty()) {
+                while (!minHeap.empty() && minHeap.top().first <= start) {
+                    currentPassengers = currentPassengers - minHeap.top().second;
+                    minHeap.pop();
+                }
             }
-            if(currPass>capacity)
-            return false;
+
+            // Step 4: Pick up new passengers
+            currentPassengers = currentPassengers + passengers;
+
+            // Step 5: Check if capacity is exceeded
+            if (currentPassengers > capacity) {
+                return false;
+            }
+
+            // Step 6: Add this trip to heap
+            // so we know when these passengers will leave
+            minHeap.push({end, passengers});
         }
+
+        // If capacity was never exceeded
         return true;
     }
 };
