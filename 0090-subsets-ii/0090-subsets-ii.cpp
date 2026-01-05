9a1b4b33c4f6fe5c9e9780d4549582511e49ed06
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2026-01-06 00:50:43 +0530

    Time: 3 ms (34.6%), Space: 17 MB (6.95%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,57 @@
 class Solution {
+
+    // Stores all unique subsets
+    vector<vector<int>> res;
+
 public:
-    void findsubsets(int ind,vector<vector<int>> & ans, vector<int> & ds,vector<int> & nums){
-     ans.push_back(ds);
-     for(int i=ind;i<nums.size();i++){
-         if(i!=ind && nums[i]==nums[i-1])
-         continue;
-         ds.push_back(nums[i]);
-         findsubsets(i+1,ans,ds,nums);
-         ds.pop_back();
-     }
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        sort(nums.begin(),nums.end());
-        findsubsets(0,ans,ds,nums);
-        return ans;
+
+        // Step 1: Sort the array so duplicates are adjacent
+        sort(nums.begin(), nums.end());
+
+        // Step 2: Start backtracking from index 0
+        backtrack(0, {}, nums);
+
+        return res;
+    }
+
+    /*
+        backtrack(i, subset, nums)
+        ---------------------------
+        i      : current index we are deciding on
+        subset : subset built so far
+        nums   : sorted input array
+    */
+    void backtrack(int i, vector<int> subset, vector<int>& nums) {
+
+        // BASE CASE:
+        // If we have processed all elements
+        if (i == nums.size()) {
+            // Store the current subset
+            res.push_back(subset);
+            return;
+        }
+
+        // ============================
+        // CHOICE 1: TAKE nums[i]
+        // ============================
+
+        subset.push_back(nums[i]);       // include current element
+        backtrack(i + 1, subset, nums);  // move to next index
+
+        // BACKTRACK: undo the choice
+        subset.pop_back();
+
+        // ============================
+        // CHOICE 2: SKIP nums[i]
+        // ============================
+
+        // Skip ALL duplicates of nums[i]
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            i++;
+        }
+
+        // Move to next DIFFERENT element
+        backtrack(i + 1, subset, nums);
     }
 };
