9756c9ae38db34075991a9389f8adfde1aaeb02c
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2026-01-06 01:21:16 +0530

    Time: 0 ms (100%), Space: 10.4 MB (84.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,44 +2,24 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 
-        // Sort so duplicates come together
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans={{}};
 
-        // Result starts with empty subset
-        vector<vector<int>> res = {{}};
+        int startindex=0;
+        int endindex=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1])
+            startindex=endindex;
+            else
+            startindex=0;
 
-        // This tells from where to start adding new subsets
-        int startIndex = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            int endIndex = res.size();  // current size of result
-
-            // IF current number is a duplicate
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                // Only use subsets added in previous step
-                startIndex = startIndex;
+            endindex=ans.size();
+            for(int j=startindex;j<endindex;j++){
+                    vector<int> subset=ans[j];
+                    subset.push_back(nums[i]);
+                    ans.push_back(subset);
             }
-            // ELSE current number is NOT a duplicate
-            else {
-                // Use all existing subsets
-                startIndex = 0;
-            }
-
-            // Save current size before adding new subsets
-            int oldSize = res.size();
-
-            // Add current number to required subsets
-            for (int j = startIndex; j < oldSize; j++) {
-                vector<int> subset = res[j];
-                subset.push_back(nums[i]);
-                res.push_back(subset);
-            }
-
-            // Update startIndex for next iteration
-            startIndex = oldSize;
         }
-
-        return res;
+        return ans;
     }
 };
