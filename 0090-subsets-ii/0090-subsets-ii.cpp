bcde54d41411bf9e98a9f41d810aed711d49056e
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2026-01-06 00:59:26 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.19%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,31 +2,42 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 
-        // Step 1: Sort to bring duplicates together
+        // Sort so duplicates come together
         sort(nums.begin(), nums.end());
 
         // Result starts with empty subset
         vector<vector<int>> res = {{}};
 
-        // prevIdx remembers where new subsets started last time
-        int prevIdx = 0;
+        // This tells from where to start adding new subsets
+        int startIndex = 0;
 
         for (int i = 0; i < nums.size(); i++) {
 
-            // If current number is duplicate,
-            // only extend subsets created in last step
-            int idx = (i > 0 && nums[i] == nums[i - 1]) ? prevIdx : 0;
+            int endIndex = res.size();  // current size of result
+
+            // IF current number is a duplicate
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // Only use subsets added in previous step
+                startIndex = startIndex;
+            }
+            // ELSE current number is NOT a duplicate
+            else {
+                // Use all existing subsets
+                startIndex = 0;
+            }
 
             // Save current size before adding new subsets
-            prevIdx = res.size();
+            int oldSize = res.size();
 
-            // Create new subsets
-            for (int j = idx; j < prevIdx; j++) {
-
+            // Add current number to required subsets
+            for (int j = startIndex; j < oldSize; j++) {
                 vector<int> subset = res[j];
                 subset.push_back(nums[i]);
                 res.push_back(subset);
             }
+
+            // Update startIndex for next iteration
+            startIndex = oldSize;
         }
 
         return res;
