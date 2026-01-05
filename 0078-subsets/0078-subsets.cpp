a60e80fa5a0578dd45906ae0bbf3921b62161b7e
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2026-01-05 12:45:19 +0530

    Time: 0 ms (100%), Space: 10 MB (41.34%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        // This will store all subsets
-        vector<vector<int>> res;
 
-        // This stores the current subset we are building
-        vector<int> subset;
+        // Result initially contains only ONE subset: empty subset
+        vector<vector<int>> res = { {} };
 
-        // Start recursion from index 0
-        dfs(nums, 0, subset, res);
+        // Loop through each number in nums
+        for (int num : nums) {
 
-        return res;
-    }
+            // Current number of subsets
+            int size = res.size();
 
-private:
-    /*
-        dfs(nums, i, subset, res)
-        --------------------------------
-        nums   : original input array
-        i      : current index we are deciding on
-        subset : elements chosen so far
-        res    : final list of all subsets
-    */
-    void dfs(const vector<int>& nums, int i,
-             vector<int>& subset,
-             vector<vector<int>>& res) {
+            // For each existing subset
+            for (int i = 0; i < size; i++) {
 
-        // BASE CASE:
-        // If we have processed all elements
-        if (i == nums.size()) {
-            // Current subset is complete, store it
-            res.push_back(subset);
-            return;
+                // Make a copy of the current subset
+                vector<int> subset = res[i];
+
+                // Add current number to it
+                subset.push_back(num);
+
+                // Add the new subset to result
+                res.push_back(subset);
+            }
         }
 
-        // ===============================
-        // CHOICE 1: INCLUDE nums[i]
-        // ===============================
-
-        subset.push_back(nums[i]);      // take current element
-        dfs(nums, i + 1, subset, res);  // move to next index
-
-        // BACKTRACK: undo the choice
-        subset.pop_back();
-
-        // ===============================
-        // CHOICE 2: EXCLUDE nums[i]
-        // ===============================
-
-        dfs(nums, i + 1, subset, res);  // move to next index
+        return res;
     }
 };
