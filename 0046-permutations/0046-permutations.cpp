c8f68f46e11668f7611c446821c134adc99bbc3c
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2026-01-07 15:22:50 +0530

    Time: 4 ms (11.63%), Space: 11.3 MB (20.3%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-    void recur(int index,vector<vector<int>>& ans,vector<int>& nums){
-      if(index==nums.size()){
-        ans.push_back(nums);
-        return;
-      }
-
-      for(int i=index;i<nums.size();i++){
-        swap(nums[index],nums[i]);
-        recur(index+1,ans,nums);
-        swap(nums[index],nums[i]);
-      }
-    }
-
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        recur(0,ans,nums);
-        return ans;
+
+        // This will store all permutations
+        vector<vector<int>> perms;
+
+        // Start with one empty permutation: { }
+        perms.push_back(vector<int>());
+
+        // Take numbers one by one from nums
+        for (int idx = 0; idx < nums.size(); idx++) {
+
+            int num = nums[idx];
+
+            // This will store new permutations after inserting num
+            vector<vector<int>> new_perms;
+
+            // Go through each existing permutation
+            for (int j = 0; j < perms.size(); j++) {
+
+                vector<int> current = perms[j];
+
+                // Insert num into every possible position
+                for (int i = 0; i <= current.size(); i++) {
+
+                    // Make a copy of current permutation
+                    vector<int> temp = current;
+
+                    // Insert num at index i
+                    temp.insert(temp.begin() + i, num);
+
+                    // Store this new permutation
+                    new_perms.push_back(temp);
+                }
+            }
+
+            // Update perms to newly formed permutations
+            perms = new_perms;
+        }
+
+        // perms now contains all permutations
+        return perms;
     }
 };
