27a9f25df98ff0585f42993d22144149935d2a99
jsoni6363 <79662231+jsoni6363@users.noreply.github.com>
2026-01-10 16:14:30 +0530

    Time: 39 ms (62.09%), Space: 62.6 MB (67.8%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
+    void backtrack(int start,vector<int>& ds,int n,int k,vector<vector<int>>& ans){
+       if(ds.size()==k){
+        ans.push_back(ds);
+        return ;
+       }
 
-    /*
-        backtrack(start)
-        ----------------
-        start -> number from where we are allowed to pick next
-        curr  -> current combination being built
-    */
-
-    void backtrack(int start, int n, int k,
-                   vector<int>& curr,
-                   vector<vector<int>>& ans) {
-
-        // ✅ If we have picked k numbers, store the combination
-        if (curr.size() == k) {
-            ans.push_back(curr);
-            return;
-        }
-
-        // ✅ Try all possible numbers from 'start' to 'n'
-        for (int num = start; num <= n; num++) {
-
-            // pick
-            curr.push_back(num);
-
-            // move forward (next number must be bigger)
-            backtrack(num + 1, n, k, curr, ans);
-
-            // backtrack (remove last picked number)
-            curr.pop_back();
-        }
+       for(int num=start;num<=n;num++){
+        ds.push_back(num);
+        backtrack(num+1,ds,n,k,ans);
+        ds.pop_back();
+       }
     }
-
     vector<vector<int>> combine(int n, int k) {
-
         vector<vector<int>> ans;
-        vector<int> curr;
-
-        // start picking from 1
-        backtrack(1, n, k, curr, ans);
-
+        vector<int> ds;
+        backtrack(1,ds,n,k,ans);
         return ans;
     }
+
 };
